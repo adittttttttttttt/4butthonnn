565db0ba5656f2cb6134ebc30584ca52ceb15e22
adittttttttttttt <105576697+adittttttttttttt@users.noreply.github.com>
2023-06-30 13:14:40 +0700

    Update channel_post.py

--- plugins/channel_post.py
+++ plugins/channel_post.py
@@ -9,7 +9,7 @@ from pyrogram.errors import FloodWait
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
 
 from bot import Bot
-from config import ADMINS, CHANNEL_ID, DISABLE_CHANNEL_BUTTON, LOGGER
+from config import ADMINS, CHANNEL_ID, DISABLE_CHANNEL_BUTTON
 from helper_func import encode
 
 
@@ -17,89 +17,46 @@ from helper_func import encode
     filters.private
     & filters.user(ADMINS)
     & ~filters.command(
-        [
-            "start",
-            "users",
-            "broadcast",
-            "ping",
-            "uptime",
-            "batch",
-            "logs",
-            "genlink",
-            "delvar",
-            "getvar",
-            "setvar",
-            "speedtest",
-            "update",
-            "stats",
-            "vars",
-            "id",
-        ]
+        ["start", "users", "broadcast", "ping", "uptime", "batch", "genlink"]
     )
 )
 async def channel_post(client: Client, message: Message):
-    reply_text = await message.reply_text("<code>Tunggu Sebentar...</code>", quote=True)
-    try:
-        post_message = await message.copy(
-            chat_id=client.db_channel.id, disable_notification=True
-        )
-    except FloodWait as e:
-        await asyncio.sleep(e.x)
-        post_message = await message.copy(
+Expand All
+	@@ -32,10 +49,10 @@ async def channel_post(client: Client, message: Message):
             chat_id=client.db_channel.id, disable_notification=True
         )
     except Exception as e:
-        LOGGER(__name__).warning(e)
+        print(e)
         await reply_text.edit_text("<b>Telah Terjadi Error...</b>")
         return
-    converted_id = post_message.id * abs(client.db_channel.id)
+    converted_id = post_message.message_id * abs(client.db_channel.id)
     string = f"get-{converted_id}"
     base64_string = await encode(string)
     link = f"https://t.me/{client.username}?start={base64_string}"
-
-    reply_markup = InlineKeyboardMarkup(
-        [
-            [
-                InlineKeyboardButton(
-                    "🔁 Share Link", url=f"https://telegram.me/share/url?url={link}"
-                )
-            ]
-        ]
-    )
-
-    await reply_text.edit(
-        f"<b>Link Sharing File Berhasil Di Buat :</b>\n\n{link}",
-        reply_markup=reply_markup,
-        disable_web_page_preview=True,
+Expand All
+	@@ -57,18 +74,19 @@ async def channel_post(client: Client, message: Message):
     )
 
     if not DISABLE_CHANNEL_BUTTON:
-        try:
-            await post_message.edit_reply_markup(reply_markup)
-        except Exception:
-            pass
+        await post_message.edit_reply_markup(reply_markup)
 
 
-@Bot.on_message(filters.channel & filters.incoming & filters.chat(CHANNEL_ID))
+@Bot.on_message(
+    filters.channel & filters.incoming & filters.chat(CHANNEL_ID) & ~filters.edited
+)
 async def new_post(client: Client, message: Message):
 
     if DISABLE_CHANNEL_BUTTON:
         return
 
-    converted_id = message.id * abs(client.db_channel.id)
+    converted_id = message.message_id * abs(client.db_channel.id)
     string = f"get-{converted_id}"
     base64_string = await encode(string)
     link = f"https://t.me/{client.username}?start={base64_string}"
-    reply_markup = InlineKeyboardMarkup(
-        [
-            [
-                InlineKeyboardButton(
-                    "🔁 Share Link", url=f"https://telegram.me/share/url?url={link}"
-                )
-            ]
-        ]
+Expand All
+	@@ -83,5 +101,5 @@ async def new_post(client: Client, message: Message):
     )
     try:
         await message.edit_reply_markup(reply_markup)
-    except Exception:
-        pass
+    except Exception as e:
+        print(e)
